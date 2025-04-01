eeb21cd370b5af4225542263317d7baba1e686db
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-04-01 21:55:01 +0530

    first commit

---
+++ Advanced/Hashing/hashing1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.HashSet;
+
+public class hashing1 {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1
+
+    }
+}
