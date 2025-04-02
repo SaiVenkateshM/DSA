d0ae917540e71ca341d6ee99e36a1cf234f3093b
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-04-02 22:32:40 +0530

    Day 1 (April 2nd) completed successfully!

--- Advanced/Hashing/hashing1.java
+++ Advanced/Hashing/hashing1.java
@@ -1,5 +1,7 @@
 package Hashing;
 
+import com.dsa.practice.everyday.Day1;
+
 import java.util.*;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -8,7 +10,7 @@ import java.util.HashSet;
 public class hashing1 {
     public static void main(String[] args) {
         HashMap<Integer, Integer> map = new HashMap<>();
-
+        Day1.main(new String[]{"String","Integer"});
 
     }
 }
--- untitled/src/main/java/com/dsa/practice/everyday/Day1.java
+++
@@ -1,7 +0,0 @@
-package com.dsa.practice.everyday;
-
-public class Day1 {
-    public static void main(String[] args) {
-
-    }
-}
