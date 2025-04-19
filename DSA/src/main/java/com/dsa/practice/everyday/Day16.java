835136c4c0a36a1a86d6fce4c7cdf0b5c40d6187
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-04-19 11:07:10 +0530

    Day 16 with 2 nd question.

--- DSA/src/main/java/com/dsa/practice/everyday/Day16.java
+++ DSA/src/main/java/com/dsa/practice/everyday/Day16.java
@@ -1,7 +1,8 @@
 package com.dsa.practice.everyday;
 /*
 1. check if the tree is valid bst(Binary search tree)
-2.
+2. Sorted Balanced Binary Search Tree.
+
  */
 
 public class Day16 {
@@ -11,6 +12,10 @@ public class Day16 {
         A.right = new TreeNode(3);
         int answer = isValidBST(A);
         System.out.println(answer);
+
+        int[] arrays = {1, 2, 3, 5, 10};
+
+        TreeNode answer1 = createBBST(arrays);
     }
 
     public static int isValidBST(TreeNode A){
@@ -29,6 +34,27 @@ public class Day16 {
         }
         return (isBST(A.left,min,A.val) && isBST(A.right,A.val,max));
 
+    }
+
+    public static TreeNode createBBST(int[] A){
+        // to make it balanced first take the middle element.
+        int len = A.length;
+        int start = 0;
+        int end = len - 1;
+
+        TreeNode answertree = balancedBST(A,start,end);
+        return answertree;
+    }
+
+    public static TreeNode balancedBST(int[] A,int start, int end){
+        if(start > end) return null;
+        int mid = (start + end)/2;
+
+        TreeNode root = new TreeNode(A[mid]);
+        root.left = balancedBST(A,start,mid -1);
+        root.right = balancedBST(A,mid+1,end);
+
+        return root;
     }
 }
 
