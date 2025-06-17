e6eff463e808bdc78e98e17c009cc3ac6a0d8868
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-06-17 09:02:23 +0530

    adding candy problem

--- DSA/src/main/java/com/dsa/practice/everyday/Day9.java
+++ DSA/src/main/java/com/dsa/practice/everyday/Day9.java
@@ -4,13 +4,17 @@ import java.util.*;
 
 /*
 1. Flipkart's Challenge in Effective Inventory Management
+2. Distribute Candy.
  */
 public class Day9 {
     public static void main(String[] args) {
-        ArrayList<Integer> A= new ArrayList<>(Arrays.asList(1,7,6,2,8,4,4,6,8,2));// expiry time
+        ArrayList<Integer> A1= new ArrayList<>(Arrays.asList(1,7,6,2,8,4,4,6,8,2));// expiry time
         ArrayList<Integer> B = new ArrayList<>(Arrays.asList(8,11,7,7,10,8,7,5,4,9));// profit
-        int answer = maxProfit(A,B);
+        int answer = maxProfit(A1,B);
         System.out.println(answer);
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1, 5, 2, 1));
+        int candyAnswer = candy(A);
+        System.out.println(candyAnswer);
     }
     public static int maxProfit(ArrayList<Integer> A, ArrayList<Integer> B){
         class Pair {
@@ -47,5 +51,28 @@ public class Day9 {
             }
         }
         return totalProfit;
+    }
+
+    public static int candy(ArrayList<Integer> A){
+       ArrayList<Integer> answerList = new ArrayList<>();
+       int answer=0;
+       for(int i : A){
+           answerList.add(1);
+       }
+        for(int i =0;i<A.size()-1;i++){
+            if(A.get(i) > A.get(i+1)){
+                answerList.set(i,answerList.get(i) + 1);
+            }
+        }
+        for(int j=A.size()-1;j>0;j--){
+            if(A.get(j) > A.get(j-1)){
+                answerList.set(j,answerList.get(j) + 1);
+            }
+        }
+        System.out.println(answerList);
+        for(int ans : answerList){
+            answer += ans;
+        }
+       return answer;
     }
 }
