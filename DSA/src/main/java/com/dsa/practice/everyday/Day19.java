69c1476ba290273a09c389a00368e81b1cf2b41f
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-07-04 13:44:06 +0530

    Graph day 3

--- DSA/src/main/java/com/dsa/practice/everyday/Day19.java
+++ DSA/src/main/java/com/dsa/practice/everyday/Day19.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 /*
 1. Another BFS
-2.
+2. Dijkstra Algorithim
  */
 public class Day19 {
     public static void main(String[] args) {
@@ -14,6 +14,14 @@ public class Day19 {
         int D = 2;
         int answer = anotherBFS(A,B,C,D);
         System.out.println(answer);
+
+        int E=6;
+        int[][] F = {{0, 4, 9},{3, 4, 6},{1, 2, 1},{2, 5, 1},{2, 4, 5},{0, 3, 7},{0, 1, 1},{4, 5, 7},{0, 5, 1}};
+        int G= 4;
+        int[] answer2 = dijkstra(E,F,G);
+        for(int i=0;i< answer2.length;i++){
+            System.out.print(answer2[i]);
+        }
     }
 
     public static int anotherBFS(int A,int[][]B,int C,int D){
@@ -62,5 +70,55 @@ public class Day19 {
             }
         }
         return -1;
+    }
+
+    public static int[] dijkstra(int A,int[][]B,int C){
+        ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
+
+        for(int i=0;i<A;i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] edge : B){
+            int u = edge[0];
+            int v = edge[1];
+            int weight = edge[2];
+            graph.get(u).add(new Pair(v,weight));
+            graph.get(v).add(new Pair(u,weight));
+        }
+
+        int[] distance = new int[A];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+        distance[C] =0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.offer(new Pair(C,0));
+
+        while(!pq.isEmpty()){
+            Pair current = pq.poll();
+            int currentNode = current.i;
+            int currentDistance = current.j;
+
+            if(distance[currentNode] != Integer.MAX_VALUE  && currentDistance >distance[currentNode]){
+                continue;
+            }
+
+            for(Pair neighbor : graph.get(currentNode)){
+                int nextNode = neighbor.i;
+                int weight = neighbor.j;
+
+                if (distance[currentNode] + weight < distance[nextNode]) {
+                    distance[nextNode] = distance[currentNode] + weight;
+                    pq.offer(new Pair(nextNode, distance[nextNode]));
+                }
+            }
+
+        }
+
+        for (int i = 0; i < A; i++) {
+            if (distance[i] == Integer.MAX_VALUE) {
+                distance[i] = -1;
+            }
+        }
+        return distance;
     }
 }
